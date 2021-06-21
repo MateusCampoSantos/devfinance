00f9bb8df6a0799f84643d93b75d8b7c84a42353
Mateus Campos <mateuscamposantos@gmail.com>
2021-06-21 08:19:06 -0300

    Add const transactions

--- scripts.js
+++ scripts.js
@@ -7,6 +7,27 @@ const Modal = {
   },
 };
 
+const transactions = [
+  {
+    id: 1,
+    descrption: "Luz",
+    amount: -50000,
+    date: "23/01/2021",
+  },
+  {
+    id: 2,
+    descrption: "Website",
+    amount: 500000,
+    date: "23/01/2021",
+  },
+  {
+    id: 3,
+    descrption: "Internet",
+    amount: -20000,
+    date: "23/01/2021",
+  },
+];
+
 const Transaction = {
   incomes() {
     //Somar as entradas
